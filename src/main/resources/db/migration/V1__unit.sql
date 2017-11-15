040968b52b16c689571c697ba62aecd7f52487fc
Kurkin Nickolay <mrnickrus@gmail.com>
2017-11-15 18:31:44 +0300

    sdfg

--- src/main/resources/db/migration/V1__unit.sql
+++ src/main/resources/db/migration/V1__unit.sql
@@ -60,67 +60,7 @@ CREATE TABLE IF NOT EXISTS vote (
 
 
 
-CREATE UNIQUE INDEX IF NOT EXISTS u_nickname_uidx
-  ON users (nickname);
-
-CREATE UNIQUE INDEX IF NOT EXISTS u_email_uidx
-  ON users (email);
-
-CREATE INDEX IF NOT EXISTS u_nickname_email_idx
-  ON users (nickname, email);
-
-CREATE INDEX IF NOT EXISTS u_nickname_id_idx ON users (nickname, id);
-
-
-
-CREATE INDEX IF NOT EXISTS f_slug_id_idx ON forum(slug, id);
-
-CREATE INDEX IF NOT EXISTS p_parent_t_id_id_idx
-  ON post (id, thread, parent);
-
-CREATE INDEX IF NOT EXISTS p_parent_t_id_idx
-  ON post (parent, thread);
-
-CREATE INDEX IF NOT EXISTS p_forum_slug
-  ON post (forum);
-
-CREATE INDEX IF NOT EXISTS p_thread_id_id_idx
-  ON post (thread, id);
-
-CREATE INDEX IF NOT EXISTS p_thread_id_path_idx
-  ON post (thread, path);
-
-CREATE INDEX IF NOT EXISTS p_parent_t_id_path_idx
-  ON post (parent, thread, path);
-
-CREATE INDEX IF NOT EXISTS p_path_t_id_idx
-  ON post (path, thread);
-
-CREATE INDEX IF NOT EXISTS p_path_created_idx
-  ON post (path, created);
 
 CREATE UNIQUE INDEX IF NOT EXISTS v_user_id_thread_id_uindex
   ON vote (nickname, threadID);
-
-
-CREATE UNIQUE INDEX IF NOT EXISTS t_slug_uindex
-  ON thread (slug);
-
-CREATE INDEX IF NOT EXISTS t_forum_id_idx
-  ON thread (forum);
-
-CREATE INDEX IF NOT EXISTS t_created_idx
-  ON thread (created);
-
-CREATE INDEX IF NOT EXISTS t_created_idx
-  ON thread (forum, created);
-
-CREATE INDEX IF NOT EXISTS p_id_f_slug_idx
-  ON post (id, forum);
-
-CREATE INDEX IF NOT EXISTS p_id_t_id_idx
-  ON post (id, thread);
-
-CREATE INDEX IF NOT EXISTS p_id_au_id_idx
-  ON post (id, author);
 
